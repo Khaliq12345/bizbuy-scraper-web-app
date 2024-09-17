ec35b5e76532f5a206f3db1ed3945218d9e5827a
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2024-09-17 23:51:11 +0100

    Edit login feature

--- pages/login_page.py
+++ pages/login_page.py
@@ -11,44 +11,56 @@ class Login_Page:
     def __init__(self) -> None:
         self.username: str = None
         self.password: str = None
+        
     
     def login_backend(self):
         self.spinner.visible = True
+        print(self.username, self.password)
         try:
-            # supabase.auth.sign_in_with_password({
-            #     'email': self.username,
-            #     'password': self.password
-            # })
             if (self.username == config.username) and (self.password == config.password): 
                 print("Logged IN")
                 app.storage.user['user'] = self.username
                 app.storage.user['expires'] = parse("In 24 hours").isoformat()
                 #{'user': self.username, 'on': True, 'expires': parse("In 24 hours").isoformat()}
-                ui.notification(
+                self.send_notif(
                     "Logged IN. You will be redirected shortly", 
-                    close_button=True, 
-                    type='positive', 
-                    position='top'
+                    n_type='positive', 
                 )
                 ui.navigate.to('/')
                 return True
-        except:
-            ui.notification(
-                "Wrong credentials", 
-                close_button=True, 
-                type='negative', 
-                position='top'
+            else:
+                self.send_notif(
+                    "Wrong credentials", 
+                    n_type='negative',
+                )
+                return None
+        except Exception as e:
+            self.send_notif(
+                e, 
+                n_type='negative',
             )
             return None
         finally:
             self.spinner.visible = False
             print(app.storage.user.get('user'))
+       
+            
+    def send_notif(self, msg, n_type='info'):
+        with self.content_col:
+            ui.notification(
+                msg, 
+                close_button=True, 
+                type=n_type, 
+                position='top'
+            )
+        
         
     def page_body(self):
         with ui.element('div').classes('grid grid-rows-4 grid-flow-col w-full h-screen'):
             with ui.element('div').classes('row-span-2 row-start-2'):
                 with ui.element('div').classes('grid grid-cols-4 w-full'):
                     self.content_col = ui.column(align_items='center').classes('col-span-2 col-start-2 gap-1')
+    
     
     def content_ui(self):
         with self.content_col:
@@ -57,8 +69,8 @@ class Login_Page:
                 self.spinner = ui.spinner(size='10px', type='dots').classes('flex justify-center w-full')
                 self.spinner.visible = False
                 with ui.item():
-                    ui.input(label='email').props('outlined stack-label')\
-                    .classes('w-full').bind_value(self, 'email')
+                    ui.input(label='username').props('outlined stack-label')\
+                    .classes('w-full').bind_value(self, 'username')
                 with ui.item():
                     ui.input(label='Password', password=True, password_toggle_button=True)\
                     .props('outlined stack-label').classes('w-full').bind_value(self, 'password')
@@ -66,6 +78,7 @@ class Login_Page:
                     ui.button("Login").props('unelevated')\
                     .classes('rounded-lg').on_click(
                         self.login_backend)
+          
             
     def main(self):
         ui.colors(
